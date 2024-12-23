63d70f4fdc5909ae38efd1afa59d75871666a16a
jnoorchashm37 <joseph@sorellalabs.xyz>
2024-12-23 00:34:46 -0500

    testing

--- src/apis/node_api.rs
+++ src/apis/node_api.rs
@@ -4,16 +4,16 @@ use angstrom_rpc::types::{
     OrderSubscriptionFilter, OrderSubscriptionKind, OrderSubscriptionResult,
 };
 use angstrom_types::orders::{CancelOrderRequest, OrderLocation, OrderStatus};
-use jsonrpsee_http_client::HttpClient;
-use std::collections::HashSet;
-
 use angstrom_types::sol_bindings::grouped_orders::AllOrders;
 use futures::{Stream, StreamExt, TryStreamExt};
+use jsonrpsee_http_client::HttpClient;
+use std::collections::HashSet;
+use validation::order::OrderPoolNewOrderResult;
 
 pub trait AngstromNodeApi {
     fn rpc_provider(&self) -> HttpClient;
 
-    async fn send_order(&self, order: AllOrders) -> eyre::Result<bool> {
+    async fn send_order(&self, order: AllOrders) -> eyre::Result<OrderPoolNewOrderResult> {
         let provider = self.rpc_provider();
         Ok(provider.send_order(order).await?)
     }
@@ -61,7 +61,10 @@ pub trait AngstromNodeApi {
             .into_stream())
     }
 
-    async fn send_orders(&self, orders: Vec<AllOrders>) -> eyre::Result<Vec<bool>> {
+    async fn send_orders(
+        &self,
+        orders: Vec<AllOrders>,
+    ) -> eyre::Result<Vec<OrderPoolNewOrderResult>> {
         let provider = self.rpc_provider();
         Ok(provider.send_orders(orders).await?)
     }
@@ -105,10 +108,13 @@ pub trait AngstromNodeApi {
 mod tests {
     #![allow(unused)]
 
+    use alloy_primitives::U256;
+    use angstrom_types::sol_bindings::RawPoolOrder;
     use angstrom_types::sol_bindings::{
         grouped_orders::{FlashVariants, GroupedVanillaOrder, StandingVariants},
         rpc_orders::TopOfBlockOrder,
     };
+
     use testing_tools::order_generator::GeneratedPoolOrders;
 
     use crate::test_utils::{make_generator, spawn_angstrom_provider, spawn_ws_provider};
@@ -153,10 +159,146 @@ mod tests {
 
         let tob_order = AllOrders::TOB(get_tob_order(&orders));
         let tob_order_sent = angstrom_provider.send_order(tob_order).await.unwrap();
-        assert!(tob_order_sent);
+        assert!(tob_order_sent.is_valid());
 
         let user_order = AllOrders::Flash(get_flash_order(&orders));
         let user_order_sent = angstrom_provider.send_order(user_order).await.unwrap();
-        assert!(user_order_sent);
+        assert!(user_order_sent.is_valid());
+    }
+
+    #[tokio::test]
+    async fn test_pending_order() {
+        let eth_provider = spawn_ws_provider().await.unwrap();
+        let angstrom_provider = spawn_angstrom_provider().await.unwrap();
+
+        let generator = make_generator(&eth_provider).await.unwrap();
+        let orders = generator.generate_orders();
+
+        let tob_order = AllOrders::TOB(get_tob_order(&orders));
+        let tob_order_sent = angstrom_provider
+            .send_order(tob_order.clone())
+            .await
+            .unwrap();
+        assert!(tob_order_sent.is_valid());
+        let pending_tob_order = angstrom_provider
+            .pending_order(tob_order.from())
+            .await
+            .unwrap();
+        assert_eq!(vec![tob_order.clone()], pending_tob_order);
+
+        let user_order = AllOrders::Flash(get_flash_order(&orders));
+        let user_order_sent = angstrom_provider
+            .send_order(user_order.clone())
+            .await
+            .unwrap();
+        assert!(user_order_sent.is_valid());
+        let pending_user_orders = angstrom_provider
+            .pending_order(user_order.from())
+            .await
+            .unwrap();
+        assert_eq!(vec![user_order.clone()], pending_user_orders);
+    }
+
+    #[tokio::test]
+    async fn test_cancel_order() {
+        let eth_provider = spawn_ws_provider().await.unwrap();
+        let angstrom_provider = spawn_angstrom_provider().await.unwrap();
+
+        let generator = make_generator(&eth_provider).await.unwrap();
+        let orders = generator.generate_orders();
+
+        let tob_order = AllOrders::TOB(get_tob_order(&orders));
+        let tob_order_sent = angstrom_provider
+            .send_order(tob_order.clone())
+            .await
+            .unwrap();
+        assert!(tob_order_sent.is_valid());
+        let canceled_tob_order = angstrom_provider
+            .cancel_order(CancelOrderRequest {
+                signature: tob_order.order_signature().unwrap(),
+                user_address: tob_order.from(),
+                order_id: tob_order.order_hash(),
+            })
+            .await
+            .unwrap();
+        assert!(canceled_tob_order);
+
+        let user_order = AllOrders::Flash(get_flash_order(&orders));
+        let user_order_sent = angstrom_provider
+            .send_order(user_order.clone())
+            .await
+            .unwrap();
+        assert!(user_order_sent.is_valid());
+        let canceled_user_orders = angstrom_provider
+            .cancel_order(CancelOrderRequest {
+                signature: user_order.order_signature().unwrap(),
+                user_address: user_order.from(),
+                order_id: user_order.order_hash(),
+            })
+            .await
+            .unwrap();
+        assert!(canceled_user_orders);
+    }
+
+    #[tokio::test]
+    async fn test_estimate_gas() {
+        let eth_provider = spawn_ws_provider().await.unwrap();
+        let angstrom_provider = spawn_angstrom_provider().await.unwrap();
+
+        let generator = make_generator(&eth_provider).await.unwrap();
+        let orders = generator.generate_orders();
+
+        let tob_order = AllOrders::TOB(get_tob_order(&orders));
+        let tob_order_gas_estimation = angstrom_provider.estimate_gas(tob_order).await.unwrap();
+        assert_eq!(
+            tob_order_gas_estimation,
+            GasEstimateResponse {
+                gas_units: 0,
+                gas: U256::ZERO
+            }
+        );
+
+        let user_order = AllOrders::Flash(get_flash_order(&orders));
+        let user_order_gas_estimation = angstrom_provider.estimate_gas(user_order).await.unwrap();
+        assert_eq!(
+            user_order_gas_estimation,
+            GasEstimateResponse {
+                gas_units: 0,
+                gas: U256::ZERO
+            }
+        );
+    }
+
+    #[tokio::test]
+    async fn test_order_status() {
+        let eth_provider = spawn_ws_provider().await.unwrap();
+        let angstrom_provider = spawn_angstrom_provider().await.unwrap();
+
+        let generator = make_generator(&eth_provider).await.unwrap();
+        let orders = generator.generate_orders();
+
+        let tob_order = AllOrders::TOB(get_tob_order(&orders));
+        let tob_order_sent = angstrom_provider
+            .send_order(tob_order.clone())
+            .await
+            .unwrap();
+        assert!(tob_order_sent.is_valid());
+        let status_tob_order = angstrom_provider
+            .order_status(tob_order.order_hash())
+            .await
+            .unwrap();
+        assert_eq!(status_tob_order, Some(OrderStatus::Pending));
+
+        let user_order = AllOrders::Flash(get_flash_order(&orders));
+        let user_order_sent = angstrom_provider
+            .send_order(user_order.clone())
+            .await
+            .unwrap();
+        assert!(user_order_sent.is_valid());
+        let status_user_order = angstrom_provider
+            .order_status(user_order.order_hash())
+            .await
+            .unwrap();
+        assert_eq!(status_user_order, Some(OrderStatus::Pending));
     }
 }
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -10,16 +10,16 @@ mod config;
 pub use config::*;
 
 pub const CONTROLLER_V1_ADDRESS: alloy_primitives::Address =
-    alloy_primitives::address!("3e23A27432444df19af7DF4751C5f2c079fD37FD");
+    alloy_primitives::address!("8c7AAa1d34ea02C2CaDe2B5d073F08AC9b099635");
 
 pub const ANGSTROM_ADDRESS: alloy_primitives::Address =
-    alloy_primitives::address!("cd33550Fa13D627172c2E341d2EA9a9896fcEa80");
+    alloy_primitives::address!("7be4551c5C8Fc657fC2bD0E26Bd040A8CD4EaA80");
 
 pub const POOL_GATE_ADDRESS: alloy_primitives::Address =
-    alloy_primitives::address!("30EAD627Ff1b44B40a1Fcb54E27BEc2b399f5b3C");
+    alloy_primitives::address!("8eb07C10398eB1918FEF1b781A1a618fdbaF89cE");
 
 pub const POOL_MANAGER_ADDRESS: alloy_primitives::Address =
-    alloy_primitives::address!("3ac4c9Ef19E9E9892daCe5B27a3ee4D9Be46D6aF");
+    alloy_primitives::address!("dC4AD216394e7C80BB2282E1fd12a0dFF3EA94Bb");
 
 pub const ANGSTROM_DEPLOYED_BLOCK: u64 = 0;
 pub const POOL_CONFIG_STORE_SLOT: u8 = 2;
