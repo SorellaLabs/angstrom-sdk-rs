2285c9ed691e3ab6b9228a62a1b8d6a5179c1653
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-01-10 16:28:35 -0500

    filler

--- src/js_utils/types/fillers.rs
+++ src/js_utils/types/fillers.rs
@@ -1,5 +1,8 @@
+use alloy_provider::Provider;
 use angstrom_sdk_macros::NeonObject;
 use neon::object::Object;
+
+use crate::{providers::AngstromProvider, types::fillers::FillWrapper};
 
 #[derive(Clone, Debug, NeonObject)]
 pub struct ClientFillerTypes {
