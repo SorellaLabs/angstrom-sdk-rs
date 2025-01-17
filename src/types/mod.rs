375db39404c7893b10384bf08bf24b9784e996b3
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-01-17 13:46:21 -0500

    wip

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -7,16 +7,16 @@ pub use historical_order_filters::*;
 pub mod fillers;
 
 pub const CONTROLLER_V1_ADDRESS: alloy_primitives::Address =
-    alloy_primitives::address!("8a1575eF61d7152d73Cf00f7Df34d592628c606A");
+    alloy_primitives::address!("0xd99a23ECF12FD411660Be733caAe736777206011");
 
 pub const ANGSTROM_ADDRESS: alloy_primitives::Address =
-    alloy_primitives::address!("26f209526dc896E3A69B761a26161791b7e3AA80");
+    alloy_primitives::address!("0x611362c23b013D1ad4Be4C018D8Ad2842d876a80");
 
 pub const POOL_GATE_ADDRESS: alloy_primitives::Address =
-    alloy_primitives::address!("3943468c83a2f8e825822Be392c27c14e8aF442F");
+    alloy_primitives::address!("0x06fE3C4D1515C4Ff139C9E544035F847EC2b5e23");
 
 pub const POOL_MANAGER_ADDRESS: alloy_primitives::Address =
-    alloy_primitives::address!("404cd02a2F55581A04B54989AdE95beF30bcEe19");
+    alloy_primitives::address!("0xC310E4d9dBF28E5e184B802E978Ab5a8E1CfCb56");
 
 pub const ANGSTROM_DEPLOYED_BLOCK: u64 = 0;
 pub const POOL_CONFIG_STORE_SLOT: u8 = 2;
