85ecdc630f5997b80594036e429bc0ba1ea5bc36
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-07-09 15:08:55 -0400

    added serde traits

--- sdk/src/types/positions/mod.rs
+++ sdk/src/types/positions/mod.rs
@@ -5,8 +5,9 @@ pub mod utils;
 
 use alloy_primitives::{U256, aliases::I24};
 use angstrom_types::contract_bindings::pool_manager::PoolManager::PoolKey;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+#[derive(Debug, Clone, Hash, PartialEq, Eq, Serialize, Deserialize)]
 pub struct UserLiquidityPosition {
     pub token_id:   U256,
     pub tick_lower: I24,
