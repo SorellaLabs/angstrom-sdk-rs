daabae4db7d8a17b2069faffd2aca6301abee96f
jnoorchashm37 <joseph@sorellalabs.xyz>
2026-02-02 13:57:58 -0500

    updated storage slot fetcher

--- sdk/src/types/providers/alloy_provider.rs
+++ sdk/src/types/providers/alloy_provider.rs
@@ -8,6 +8,7 @@ use alloy_provider::{Provider, RootProvider};
 use alloy_rpc_types::{TransactionInput, TransactionRequest};
 use alloy_sol_types::{SolCall, SolType};
 use alloy_transport::TransportErrorKind;
+use uniswap_storage::StorageSlotFetcher;
 
 /// Wrapper for alloy providers that implements SDK traits.
 /// This wrapper is necessary to avoid trait coherence conflicts with
@@ -15,7 +16,7 @@ use alloy_transport::TransportErrorKind;
 #[derive(Clone)]
 pub struct AlloyProviderWrapper<P, N: Network = Ethereum> {
     provider: P,
-    _network: PhantomData<N>
+    _network: PhantomData<N>,
 }
 
 impl<P: Debug, N: Network> Debug for AlloyProviderWrapper<P, N> {
@@ -64,11 +65,11 @@ pub(crate) async fn alloy_view_call<P, IC>(
     provider: &P,
     block_number: Option<u64>,
     contract: Address,
-    call: IC
+    call: IC,
 ) -> Result<Result<IC::Return, alloy_sol_types::Error>, RpcError<TransportErrorKind>>
 where
     P: Provider + Clone,
-    IC: SolCall + Send
+    IC: SolCall + Send,
 {
     let tx = TransactionRequest {
         to: Some(TxKind::Call(contract)),
@@ -86,16 +87,32 @@ where
 pub(crate) async fn alloy_view_deploy<P, N, IC>(
     provider: &P,
     block_number: Option<u64>,
-    tx: <N as Network>::TransactionRequest
+    tx: <N as Network>::TransactionRequest,
 ) -> Result<Result<IC::RustType, alloy_sol_types::Error>, RpcError<TransportErrorKind>>
 where
     P: Provider<N>,
     N: Network,
-    IC: SolType + Send
+    IC: SolType + Send,
 {
     let data = provider
         .call(tx)
         .block(block_number.map(Into::into).unwrap_or(BlockId::latest()))
         .await?;
     Ok(IC::abi_decode(&data))
+}
+
+#[async_trait::async_trait]
+impl<P: Provider<N>, N: Network> StorageSlotFetcher for AlloyProviderWrapper<P, N: Network> {
+    async fn storage_at(
+        &self,
+        address: Address,
+        key: StorageKey,
+        block_number: Option<u64>,
+    ) -> eyre::Result<StorageValue> {
+        Ok(self
+            .root()
+            .get_storage_at(address, key.into())
+            .block_id(block_number.map(Into::into).unwrap_or(BlockId::latest()))
+            .await?)
+    }
 }
