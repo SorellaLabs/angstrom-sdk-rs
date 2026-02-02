f1c46d3cde294b351118f3a6368ae7f41af09b52
jnoorchashm37 <joseph@sorellalabs.xyz>
2026-02-02 14:47:11 -0500

    updated versions

--- sdk/src/types/fees/uniswap.rs
+++ sdk/src/types/fees/uniswap.rs
@@ -3,8 +3,8 @@ use angstrom_types_primitives::primitive::PoolId;
 use uniswap_storage::{
     self, StorageSlotFetcher,
     v4::pool_manager::position_state::{
-        pool_manager_position_fee_growth_inside, pool_manager_position_state_last_fee_growth_inside
-    }
+        pool_manager_position_fee_growth_inside, pool_manager_position_state_last_fee_growth_inside,
+    },
 };
 
 pub async fn uniswap_fee_deltas<F: StorageSlotFetcher>(
@@ -16,11 +16,11 @@ pub async fn uniswap_fee_deltas<F: StorageSlotFetcher>(
     current_pool_tick: I24,
     position_token_id: U256,
     tick_lower: I24,
-    tick_upper: I24
+    tick_upper: I24,
 ) -> eyre::Result<(U256, U256)> {
     let (
         (fee_growth_inside0_x128, fee_growth_inside1_x128),
-        (fee_growth_inside0_last_x128, fee_growth_inside1_last_x128)
+        (fee_growth_inside0_last_x128, fee_growth_inside1_last_x128),
     ) = tokio::try_join!(
         pool_manager_position_fee_growth_inside(
             slot_fetcher,
@@ -45,7 +45,7 @@ pub async fn uniswap_fee_deltas<F: StorageSlotFetcher>(
 
     Ok((
         fee_growth_inside0_x128 - fee_growth_inside0_last_x128,
-        fee_growth_inside1_x128 - fee_growth_inside1_last_x128
+        fee_growth_inside1_x128 - fee_growth_inside1_last_x128,
     ))
 }
 
@@ -63,7 +63,7 @@ mod tests {
         let block_number = pos_info.block_number;
 
         #[cfg(feature = "local-reth")]
-        let provider = provider.provider_ref();
+        let provider = &provider.provider_ref().eth_api();
         #[cfg(not(feature = "local-reth"))]
         let provider = &provider;
 
@@ -76,7 +76,7 @@ mod tests {
             pos_info.current_pool_tick,
             pos_info.position_token_id,
             pos_info.tick_lower,
-            pos_info.tick_upper
+            pos_info.tick_upper,
         )
         .await
         .unwrap();
@@ -111,7 +111,7 @@ mod tests {
             pos_info.current_pool_tick,
             pos_info.position_token_id,
             pos_info.tick_lower,
-            pos_info.tick_upper
+            pos_info.tick_upper,
         )
         .await
         .unwrap();
